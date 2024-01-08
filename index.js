72f639c53792effce42769e20e8b8ad1951dea51
Fran <fducekuce@gmail.com>
2024-01-08 17:07:12 +0100

    fix old bugs

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const express = require('express')
-const app = express();
-const mongoose = require('mongoose');
-const Equipment = require('./model/equipment')
-const methodoverride = require('method-override')
-const catchAsync = require('./utils/catchAsync')
-const ExpressError = require('./utils/expressError');
-const equipmentRouter = require('./routes/equipment')
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const Equipment = require("./model/equipment")
+const methodoverride = require("method-override")
+const catchAsync = require("./utils/catchAsync")
+const ExpressError = require("./utils/expressError")
+const equipmentRouter = require("./routes/equipment")
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err))
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/zajebansija');
-    console.log('Mongoose is ON!')
+  await mongoose.connect("mongodb://127.0.0.1:27017/zajebansija")
+  console.log("Mongoose is ON!")
 }
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs")
 
 app.use(express.urlencoded({ extended: true }))
-app.use(methodoverride('_method'))
+app.use(methodoverride("_method"))
 
-app.use('/equipment', equipmentRouter)
+app.use("/", equipmentRouter)
 
-app.get('/', (req, res) => {
-    res.send('Home page working')
-})
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError('page not found', 404))
+app.all("/weww/ww", (req, res, next) => {
+  next(new ExpressError("page not found", 404))
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'wrong way' } = err
-    res.status(statusCode).send(message)
+  const { statusCode = 500, message = "wrong way" } = err
+  res.status(statusCode).send(message)
 })
 
 app.listen(8080, (req, res) => {
-    console.log('Express is ON!')
+  console.log("Express is ON!")
 })
--- routes/equipment.js
+++ routes/equipment.js
@@ -1,45 +1,53 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const catchAsync = require('../utils/catchAsync')
-const Equipment = require('../model/equipment')
+const catchAsync = require("../utils/catchAsync")
+const Equipment = require("../model/equipment")
 
-router.get('/', async (req, res) => {
-    const equipment = await Equipment.find({})
-    res.render('index', { equipment })
+router.get("/", async (req, res) => {
+  const equipment = await Equipment.find({})
+  res.render("index", { equipment })
 })
 
-router.get('/new', (req, res) => {
-    res.render('new')
+router.get("/new", (req, res) => {
+  res.render("new")
 })
 
-router.get('/:id', catchAsync(async (req, res) => {
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
     const { id } = req.params
     const equip = await Equipment.findById(id)
-    res.render('show', { equip })
-}))
+    res.render("show", { equip })
+  })
+)
 
-router.post('/new', catchAsync(async (req, res, next) => {
+router.post(
+  "/new",
+  catchAsync(async (req, res, next) => {
     const equipment = new Equipment(req.body.equipment)
     await equipment.save()
-    res.redirect(`/equipment/${equipment._id}`)
-}))
+    res.redirect(`/${equipment._id}`)
+  })
+)
 
-router.get('/:id/edit', async (req, res) => {
-    const { id } = req.params
-    const equip = await Equipment.findById(id)
-    res.render('edit', { equip })
+router.get("/:id/edit", async (req, res) => {
+  const { id } = req.params
+  const equip = await Equipment.findById(id)
+  res.render("edit", { equip })
 })
 
-router.put('/:id', async (req, res) => {
-    const { id } = req.params
-    const newEquip = await Equipment.findByIdAndUpdate(id, { ...req.body.equipment })
-    res.redirect('/equipment')
+router.put("/:id", async (req, res) => {
+  const { id } = req.params
+  const newEquip = await Equipment.findByIdAndUpdate(id, {
+    ...req.body.equipment,
+  })
+  res.redirect("/")
 })
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    await Equipment.findByIdAndDelete(id)
-    res.redirect('/equipment')
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params
+  await Equipment.findByIdAndDelete(id)
+  res.redirect("/")
 })
 
 module.exports = router
